de1b8260681d54cc1d5c8ec5ad7dd0ab0f9e85d9
micahp0506 <micahp0506@gmail.com>
2015-11-06 14:43:41 -0600

    Added cat JSON

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
 var newDogArray = [];
 var newDogTypeArray = [];
+var newDogVolumeArray = [];
 
-// Dog
+// Dogfood
 function dogIWantToAdd(dog) {
 	for (var i = 0; i < dog.dog_brands.length; i++) {
-		console.log("2", dog.dog_brands[i]);
+		console.log("dog2", dog.dog_brands[i]);
 		newDogArray[i] = "<p>" + dog.dog_brands[i].brand + "</p>";
 		$("#output").append(newDogArray[i]);
 		dogTypesToAdd(dog.dog_brands[i]);	
@@ -16,7 +17,7 @@ function dogIWantToAdd(dog) {
 
 function dogTypesToAdd(dog) {
 	for (var j = 0; j < dog.types.length; j++) {
-		console.log("3", dog.types[j]);
+		console.log("dog3", dog.types[j]);
 		newDogTypeArray[j] = "<p>" + dog.types[j].type + "</p>";
 		$("#output").append(newDogTypeArray[j]);
 		dogVolumesToAdd(dog.types[j]);
@@ -25,9 +26,9 @@ function dogTypesToAdd(dog) {
 
 function dogVolumesToAdd(dog) {
 	for (var z = 0; z < dog.volumes.length; z++) {
-	console.log("3", dog.volumes[z]);
-	newDogTypeArray[z] = "<p>" + dog.volumes[z].volume + dog.volumes[z].price + "</p>";
-	$("#output").append(newDogTypeArray[z]);
+	console.log("dog4", dog.volumes[z]);
+	newDogVolumeArray[z] = "<p>" + dog.volumes[z].volume + dog.volumes[z].price + "</p>";
+	$("#output").append(newDogVolumeArray[z]);
 	}
 
 }
@@ -37,11 +38,61 @@ function dogVolumesToAdd(dog) {
 $.ajax({
 	url: "dog.json"
 }).done(function(dogObject) {
-	console.log("1", dogObject);
+	console.log("Dog1", dogObject);
 	dogIWantToAdd(dogObject);
 	
 });
 
+var newCatArray = [];
+var newCatBreedsArray = [];
+var newCatTypeArray = [];
+var newCatVolumeArray = [];
 
+// Catfood
+function catIWantToAdd(cat) {
+	for (var i = 0; i < cat.cat_brands.length; i++) {
+		console.log("cat2", cat.cat_brands[i]);
+		newCatArray[i] = "<p>" + cat.cat_brands[i].brand + "</p>";
+		$("#output").append(newCatArray[i]);
+		catBreedsToAdd(cat.cat_brands[i]);	
+		
+	}	
+}
+
+function catBreedsToAdd(cat) {
+	for (var j = 0; j < cat.breeds.length; j++) {
+		console.log("cat3", cat.breeds[j]);
+		newCatBreedsArray[j] = "<p>" + cat.breeds[j].breed + "</p>";
+		$("#output").append(newCatBreedsArray[j]);
+		catTypeToAdd(cat.breeds[j]);
+	}
+}
+
+function catTypeToAdd(cat) {
+	for (var z = 0; z < cat.types.length; z++) {
+		console.log("cat4", cat.types[z]);
+		newCatTypeArray[z] = "<p>" + cat.types[z].type + "</p>";
+		$("#output").append(newCatTypeArray[z]);
+		catVolumeToAdd(cat.types[z]);
+	}
+
+}
+
+function catVolumeToAdd(cat) {
+	for (var h = 0; h < cat.volumes.length; h++) {
+		console.log("cat5", cat.volumes[h]);
+		newCatVolumeArray[h] = "<p>" + cat.volumes[h].volume + cat.volumes[h].price +"</p>";
+		$("#output").append(newCatVolumeArray[h]);
+	}
+}
+
+
+$.ajax({
+	url: "cat.json"
+}).done(function(catObject) {
+	console.log("Cat1", catObject);
+	catIWantToAdd(catObject);
+	
+});
 
 });
